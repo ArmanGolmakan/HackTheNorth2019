7c7e556fccc887f9ec0361c4c072085bb1a52c38
kashyapsavaliya <kkashyap.savaliya@gmail.com>
2019-09-14 11:19:47 -0400

    Register screen changes

--- web/src/components/Register/Register.js
+++ web/src/components/Register/Register.js
@@ -1,12 +1,26 @@
 import React from "react";
 import TextInput from "../TextInput";
 import Button from "../Button";
+import AppBar from '@material-ui/core/AppBar';
+import TextField from '@material-ui/core/TextField';
+import RaisedButton from '@material-ui/core/Button';
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = { inputText: "" };
   }
+
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
   textInputChange = event => {
     this.setState({
       inputText: event.target.value
@@ -18,13 +32,53 @@ class Register extends React.Component {
   };
   state = {};
   render() {
+    const { values, textInputChange } = this.props;
     return (
-      <div>
-        <TextInput onChange={this.textInputChange} />
-        <Button onClick={this.onSubmitButtonClick} />
-      </div>
+        <React.Fragment>
+          <AppBar title="Enter Personal Details" />
+          <TextField
+            hintText="Enter Your Occupation"
+            floatingLabelText="Occupation"
+            onChange={this.textInputChange('occupation')}
+            defaultValue={values.occupation}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your City"
+            floatingLabelText="City"
+            onChange={this.textInputChange('city')}
+            defaultValue={values.city}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Bio"
+            floatingLabelText="Bio"
+            onChange={this.textInputChange('bio')}
+            defaultValue={values.bio}
+          />
+          <br />
+          <RaisedButton
+            label="Continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
+          <RaisedButton
+            label="Back"
+            primary={false}
+            style={styles.button}
+            onClick={this.back}
+          />
+        </React.Fragment>
+     
     );
   }
 }
+
+const styles = {
+  button: {
+    margin: 15
+  }
+};
 
 export default Register;
