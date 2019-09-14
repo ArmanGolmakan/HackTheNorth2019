d75de0663125cf19b9f9c62c33c850b07317fb29
Gian Alix <gian.alix@gmail.com>
2019-09-14 14:54:21 -0400

    merged my code with arman

--- web/src/components/Register/Register.js
+++ web/src/components/Register/Register.js
@@ -38,74 +38,17 @@ class Register extends React.Component {
     } = this.state;
     return (
       <div>
-<<<<<<< HEAD
-        {/* <TextField
-          onChange={e => {
-            this.textInputChange(e, "name");
-          }}
-          defaultValue={name}
-        />
-        <TextField
-          onChange={e => {
-            this.textInputChange(e, "age");
-          }}
-          defaultValue={age}
-        />
-        <TextField
-          onChange={e => {
-            this.textInputChange(e, "emergencyContact");
-          }}
-          defaultValue={emergencyContact}
-        />
-        <TextField
-          onChange={e => {
-            this.textInputChange(e, "allergies");
-          }}
-          defaultValue={allergies}
-        />
-        <TextField
-          onChange={e => {
-            this.textInputChange(e, "bloodType");
-          }}
-          defaultValue={bloodType}
-        />
-        <Button
-          label="Submit"
-          primary={true}
-          style={styles.button}
-          onClick={this.onSubmitButtonClick}
-        /> */}
+
         <div className="container">
           <div className="leftTabDiv">
-            <p>New Patient</p>
-            <p>Load Patient</p>
-            <p>Settings</p>
-          </div>
-          <div className="mainDiv">
-            <div className="regFormDiv">
-              <div className="regDiv">
-                Registration
-              </div>
-              <div className="formDiv">
-                <p className="name">Name</p>
-                <p className="age">Age</p>
-                <p>Emergency Contact</p>
-                <p>Current Medications</p>
-                <p>Allergies</p>
-                <p>Blood Type</p>
-              </div>
-            </div>
-            <div className="picDiv">
-=======
-        <div className="1">
-          <div className="2">
             <div className="newPatient">New Patient</div>
             <div className="loadPatient">Load Patient</div>
             <div className="settings">Settings</div>
           </div>
-          <div className="3">
+          <div className="regFormDiv">
             <div className="4">Registration</div>
-            <div className="5">
+            <div className="formDiv">
+              <div className="reg">Registration</div>
               <input type="text" name="name" placeholder="Name" />
               <input type="text" name="emergcontact" placeholder="Emergency Contact" onChange={e => { this.textInputChange(e, "emergencyContact") }} />
               <input type="text" name="curmeds" placeholder="Current Medications" onChange={e => { this.textInputChange(e, "currentMedications") }} />
@@ -113,25 +56,18 @@ class Register extends React.Component {
               <input type="text" name="bt" placeholder="Blood Type" onChange={e => { this.textInputChange(e, "bloodType") }} />
               <input type="text" name="age" placeholder="Age" onChange={e => { this.textInputChange(e, "age") }} />
             </div>
-            <div className="6">
->>>>>>> e41b73db9f2fb7455cd61d16a236e306862ded97
-            </div>
-          </div>
+            <div className="picDiv">
+            </div> /*picDiv*/
+          </div> 
         </div>
+      </div>
 
-        <Button
+        {/*<Button
           label="Submit"
           primary={true}
-          onClick={this.onSubmitButtonClick}>Submit</Button>
-
-      </div>
+          onClick={this.onSubmitButtonClick}>Submit</Button>*/}
     );
   }
 }
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> e41b73db9f2fb7455cd61d16a236e306862ded97
 export default Register;
