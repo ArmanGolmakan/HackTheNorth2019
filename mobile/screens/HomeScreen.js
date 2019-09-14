5f70d4b6623b164430e1453e45a70cb9d4c83dc7
Juyoung Park <james.park.victoria@gmail.com>
2019-09-14 17:33:02 -0400

    Implemented firebase upload from mobile

--- mobile/screens/HomeScreen.js
+++ mobile/screens/HomeScreen.js
@@ -2,19 +2,20 @@ import React from "react";
 import { View, Text, Button, Alert } from "react-native";
 import * as firebase from "firebase";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCQAALH6GDc6in0lH6x_Prmuvk1BkR_3qw",
+  authDomain: "hackthenorth2019-6706e.firebaseapp.com",
+  databaseURL: "https://hackthenorth2019-6706e.firebaseio.com",
+  projectId: "hackthenorth2019-6706e",
+  storageBucket: "",
+  messagingSenderId: "783964043403",
+  appId: "1:783964043403:web:9f87c784d00cf3277b3c8e"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const userRef = firebaseApp.database().ref("user/");
+
 class HomeScreen extends React.Component {
   componentWillMount() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyCQAALH6GDc6in0lH6x_Prmuvk1BkR_3qw",
-      authDomain: "hackthenorth2019-6706e.firebaseapp.com",
-      databaseURL: "https://hackthenorth2019-6706e.firebaseio.com",
-      projectId: "hackthenorth2019-6706e",
-      storageBucket: "",
-      messagingSenderId: "783964043403",
-      appId: "1:783964043403:web:9f87c784d00cf3277b3c8e"
-    };
-    const firebaseApp = firebase.initializeApp(firebaseConfig);
-    const userRef = firebaseApp.database().ref("user/");
     userRef.on("value", snapshot => {
       const value = JSON.stringify(snapshot.val());
       this.props.navigation.navigate("WriteNFC", { value });
@@ -27,7 +28,7 @@ class HomeScreen extends React.Component {
         <Text>Home Screen</Text>
         <Button
           onPress={() => {
-            navigate("ReadNFC");
+            navigate("ReadNFC", { firebaseApp });
           }}
           title="Scan NFC"
         />
