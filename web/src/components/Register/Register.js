02e9e807dbd6e4cbdd9f18f512eaecedde71717a
Juyoung Park <james.park.victoria@gmail.com>
2019-09-14 11:44:47 -0400

    Fixed issues for data sending

--- web/src/components/Register/Register.js
+++ web/src/components/Register/Register.js
@@ -1,83 +1,48 @@
 import React from "react";
-import TextInput from "../TextInput";
-import Button from "../Button";
-import AppBar from '@material-ui/core/AppBar';
-import TextField from '@material-ui/core/TextField';
-import RaisedButton from '@material-ui/core/Button';
+import AppBar from "@material-ui/core/AppBar";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { inputText: "" };
+    this.state = {};
   }
 
-  continue = e => {
-    e.preventDefault();
-    this.props.nextStep();
-  };
-
-  back = e => {
-    e.preventDefault();
-    this.props.prevStep();
-  };
-
-  textInputChange = event => {
+  textInputChange = (event, type) => {
     this.setState({
-      inputText: event.target.value
+      [type]: event.target.value
     });
   };
 
   onSubmitButtonClick = () => {
     // send inputText to firebase
   };
-  state = {};
   render() {
-    const { values, textInputChange } = this.props;
     return (
-        <React.Fragment>
-          <AppBar title="Enter Personal Details" />
-          <TextField
-            hintText="Enter Your Occupation"
-            floatingLabelText="Occupation"
-            onChange={this.textInputChange('occupation')}
-            defaultValue={values.occupation}
-          />
-          <br />
-          <TextField
-            hintText="Enter Your City"
-            floatingLabelText="City"
-            onChange={this.textInputChange('city')}
-            defaultValue={values.city}
-          />
-          <br />
-          <TextField
-            hintText="Enter Your Bio"
-            floatingLabelText="Bio"
-            onChange={this.textInputChange('bio')}
-            defaultValue={values.bio}
-          />
-          <br />
-          <RaisedButton
-            label="Continue"
-            primary={true}
-            style={styles.button}
-            onClick={this.continue}
-          />
-          <RaisedButton
-            label="Back"
-            primary={false}
-            style={styles.button}
-            onClick={this.back}
-          />
-        </React.Fragment>
-     
+      <React.Fragment>
+        <AppBar title="Enter Personal Details" />
+        <TextField
+          onChange={e => {
+            this.textInputChange(e, "occupation");
+          }}
+          defaultValue={"job"}
+        />
+        <Button
+          label="Submit"
+          primary={true}
+          style={styles.button}
+          onClick={this.onSubmitButtonClick}
+        />
+      </React.Fragment>
     );
   }
 }
 
 const styles = {
   button: {
-    margin: 15
+    margin: 15,
+    background: "red"
   }
 };
 
